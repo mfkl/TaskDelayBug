5f5950d9c8de36ac61b1fae641c6969d4f480ac7
Martin Finkel <me@martinfinkel.com>
2022-04-08 16:21:37 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+var delay = 5000;
+var tasks = Enumerable.Range(0, 10000000).Select(async x =>
+{
+    var sw = Stopwatch.StartNew();
+    await Task.Delay(delay);
+    var elapsed = sw.ElapsedMilliseconds;
+    if (elapsed < delay)
+        Console.WriteLine($"Supposed to wait for {delay} but instead waited for {elapsed}.");
+
+    return 0;
+});
+
+var x = await Task.WhenAll(tasks);
+Console.WriteLine(x.Sum());
